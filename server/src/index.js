e2e7a692da1e0c24e42544ab331b8537cad5b9f5
闫明琪 <13271137+yan-mingqi@user.noreply.gitee.com>
2024-12-30 04:42:07 +0800

    Update CORS headers configuration

--- server/src/index.js
+++ server/src/index.js
@@ -5,18 +5,21 @@ const config = require('./config');
 
 const app = express();
 
-// 更新 CORS 配置
-app.use(cors({
-  origin: [
-    'https://yan-mq.github.io',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}));
+// 添加自定义中间件处理 CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://yan-mq.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Content-Type-Options', 'nosniff');
+  res.header('X-Content-Type-Options', 'nosniff');
+
+  // 处理 OPTIONS 请求
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(express.json());
 
